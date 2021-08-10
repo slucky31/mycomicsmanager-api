ee14a4f82f7e1d11c84f46d59fe2361231967b30
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2021-08-11 00:03:44 +0200

    ComputerVision
    Mise en place

---
+++ ComputerVision/ComputerVisionService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace MyComicsManagerApi.ComputerVision
+{
+    public class ComputerVisionService
+    {
+
+
+        public ComputerVisionService()
+        {
+        }
+
+        public ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        public async Task<string> ReadFileUrl(ComputerVisionClient client, string urlFile)
+        {
+            
+            Log.Debug("READ FILE FROM URL");
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile);
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+            // </snippet_readfileurl_1>
+
+            // <snippet_readfileurl_2>
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Log.Debug($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+            
+            // Display the found text.            
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Log.Debug(line.Text);
+                }
+            }
+            return textUrlFileResults.ToString();
+        }
+
+        public async Task ReadFileLocal(ComputerVisionClient client, string localFile)
+        {
+            Log.Debug("READ FILE FROM LOCAL");
+
+            // Read text from URL
+            var textHeaders = await client.ReadInStreamAsync(File.OpenRead(localFile));
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the recognized text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Log.Debug($"Reading text from local file {Path.GetFileName(localFile)}...");
+
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+
+            // Display the found text.
+            Console.WriteLine();
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Log.Debug(line.Text);
+                }
+            }
+
+        }
+    }
+}
